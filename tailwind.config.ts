ca82a475bf02e1e87ed2849885475ce15d19a5d5
decimoor <chotchaev.123@live.com>
2024-07-25 19:55:00 +0300

    setting shadcn, colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,56 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+      "./pages/**/*.{js,jsx}",
+      "./components/**/*.{js,jsx}",
+      "./app/**/*.{js,jsx}",
+      "./src/**/*.{js,jsx}",
   ],
+  prefix: "",
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      container: {
+          center: true,
+          padding: "15px",
       },
-    },
+      screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "960px",
+          xl: "1200px",
+      },
+      fontFamily: {
+          primary: "var(--font-jetbrainsMono)",
+      },
+      extend: {
+          colors: {
+              primary: "#62a4a6",
+              accent: {
+                  DEFAULT: "#d9d93f",
+                  hover: "#ffff70",
+              },
+          },
+          keyframes: {
+              "accordion-down": {
+                  from: { height: "0" },
+                  to: { height: "var(--radix-accordion-content-height)" },
+              },
+              "accordion-up": {
+                  from: { height: "var(--radix-accordion-content-height)" },
+                  to: { height: "0" },
+              },
+              "disappear": {
+                  "0%": { opacity: "100%" },
+                  "99%": { opacity: "0%" },
+                  "100%": { display: "none" },
+              },
+          },
+          animation: {
+              "accordion-down": "accordion-down 0.2s ease-out",
+              "accordion-up": "accordion-up 0.2s ease-out",
+              "disappear": "disappear 0.3s ease-out forwards",
+          },
+      },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
-export default config;
